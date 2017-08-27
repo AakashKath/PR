3521de50b8f60127a24b7ecce59cc3ee6a0a334e
HakZ <aakash.kath.258@gmail.com>
2017-08-27 21:40:52 +0530

    all 3

--- Sample-2D-Data/plotclass2.py
+++ Sample-2D-Data/plotclass2.py
@@ -1,11 +1,11 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-x, y=np.loadtxt('Class2.txt', delimiter=' ', unpack=True)
+x, y=np.loadtxt('Class3.txt', delimiter=' ', unpack=True)
 plt.plot(x, y, label='Loaded from file!')
 
 plt.xlabel('x')
 plt.ylabel('y')
 plt.title('Interesting Graph\nCheck it out')
 plt.legend()
-plt.savefig(filename='Class2.png')
+plt.savefig(filename='Class3.png')
